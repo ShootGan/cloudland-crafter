a6dcafdd6715a8b2e33e18679865b31cdd6f418a
ShootGan <nawelpalepka@gmail.com>
2023-01-07 17:27:10 +0100

    fix start.sh

--- docker_images/multipaper_server/start.sh
+++ docker_images/multipaper_server/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms128M -Xmx4096M -DmultipaperMasterAddress=master-multipaper-service.deafult.svc.cluster.local:35353 -jar server.jar
+java -Xms128M -Xmx4096M -DmultipaperMasterAddress=master-multipaper-service.default.svc.cluster.local:35353 -jar server.jar
