13b2bf42aa20e7610211c343de54e7d7d316c54f
ShootGan <nawelpalepka@gmail.com>
2023-01-07 13:57:44 +0100

    init

---
+++ docker_images/multipaper_master/start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -Xms128M -Xmx512M -jar /multipaper/server.jar 35353 25565
---
+++ docker_images/multipaper_server/start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -Xms128M -Xmx4096M -DmultipaperMasterAddress=172.17.0.2:35353 -jar server.jar
