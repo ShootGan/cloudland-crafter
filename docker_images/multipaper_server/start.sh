aef80b9d8d904ba314ae75211285d241371ed4d5
ShootGan <nawelpalepka@gmail.com>
2023-01-07 15:44:21 +0100

    add slave

--- docker_images/multipaper_server/start.sh
+++ docker_images/multipaper_server/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms128M -Xmx4096M -DmultipaperMasterAddress=172.17.0.2:35353 -jar server.jar
+java -Xms128M -Xmx4096M -DmultipaperMasterAddress=master-multipaper-service.deafult.svc.cluster.local:35353 -jar server.jar
